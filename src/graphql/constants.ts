88e62526220c3416e9af82e9861ea98167b9da2a
mguleryuz <mguleryuz3@gmail.com>
2025-12-16 21:52:08 -0500

    chore: update default GraphQL URL to the latest indexer endpoint

--- src/graphql/constants.ts
+++ src/graphql/constants.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_GRAPHQL_URL = 'https://indexer.dev.hyperindex.xyz/8a9c53e/v1/graphql'
+export const DEFAULT_GRAPHQL_URL = 'https://indexer.dev.hyperindex.xyz/889d073/v1/graphql'
