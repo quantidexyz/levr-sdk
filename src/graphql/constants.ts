fbfa098b5e4458da5a360a7261c63f338abd971a
mguleryuz <mguleryuz3@gmail.com>
2025-12-31 01:03:50 -0500

    fix: update DEFAULT_GRAPHQL_URL to point to the latest indexer endpoint

--- src/graphql/constants.ts
+++ src/graphql/constants.ts
@@ -1,1 +1,2 @@
-export const DEFAULT_GRAPHQL_URL = 'https://indexer.dev.hyperindex.xyz/1f22da5/v1/graphql'
+// export const DEFAULT_GRAPHQL_URL = 'http://localhost:8080/v1/graphql'
+export const DEFAULT_GRAPHQL_URL = 'https://indexer.dev.hyperindex.xyz/80792c0/v1/graphql'
